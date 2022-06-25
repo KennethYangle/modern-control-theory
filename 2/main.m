ebde40f4b5f16abb978b57919b4cc3dd79be4ad2
kenneth <1269769296@qq.com>
2022-06-25 21:10:52 +0800

    update plot

--- 2/main.m
+++ 2/main.m
@@ -10,6 +10,7 @@ LoadParams;
 x = p.ModelParams.x0;
 x_stash = zeros(2, p.simN);
 yd_stash = zeros(1, p.simN);
+u_stash = zeros(1, p.simN);
 for i = 1: p.simN
     % 控制器
     y = x(1);
@@ -20,16 +21,31 @@ for i = 1: p.simN
     % 记录
     x_stash(:, i) = x;
     yd_stash(i) = yd;
+    u_stash(i) = u;
 end
 
 %% 画图
 if p.plotOn == true
     t = p.Ts * (1: p.simN);
     figure(1);
-    plot(t, x_stash(1,:)/1.5, t, x_stash(2,:)/1.5);
-    legend('x_1','x_2');
-    figure(2);
+    subplot(3, 1, 1);
     plot(t, x_stash(1,:)/1.5, t, yd_stash(:));
     legend('y','y_d');
+    xlabel('t / s');
+    ylabel('y');
+    grid on;
+    
+    subplot(3, 1, 2);
+    plot(t, x_stash(1,:)/1.5, t, x_stash(2,:)/1.5);
+    xlabel('t / s');
+    ylabel('x');
+    legend('x_1','x_2');
+    grid on;
+    
+    subplot(3, 1, 3);
+    plot(t, u_stash);
+    xlabel('t / s');
+    ylabel('u');
+    legend('u');
     grid on;
 end
--- 3/main.m
+++ 3/main.m
@@ -4,14 +4,14 @@ close all
 clc
 
 %% 系统模型
-A = [1.1, 2; 0, 0.95];
-B = [0; 0.079];
+A = [1, 1; 0, 1];
+B = [0; 1];
 C = eye(2);
-D = [0.1; 0.5];
+D = [1; 1];
 
 %% 控制器
 % 控制时域
-Nc = 10;
+Nc = 20;
 
 % 权重矩阵
 Q = 1;
@@ -20,7 +20,7 @@ QQ = Q * eye(Nc * 2);
 RR = R * eye(Nc * 1);
 
 % 线性反馈
-K = [2.5, 12.5];
+K = [0.4, 1.2];
 
 % 求解P
 Ak = A - B * K;
@@ -32,12 +32,12 @@ QQ(Nc*2-1:Nc*2, Nc*2-1:Nc*2) = P;
 [F, Phi] = MPCGain(A, B, Nc);
 
 % 约束
-u_upper = 4;
-u_lower = -4;
+u_upper = 1;
+u_lower = -1;
 
 % 压缩约束
-lb = (u_lower + 1.375) * ones(Nc, 1);
-ub = (u_upper - 1.375) * ones(Nc, 1);
+lb = (u_lower + 0.423) * ones(Nc, 1);
+ub = (u_upper - 0.423) * ones(Nc, 1);
 
 % 终端约束
 u1_terminal_upper = 0.1;
@@ -46,7 +46,7 @@ u1_terminal_lower = -0.1;
 u2_terminal_lower = -0.1;
 
 % 仿真周期
-total = 40;
+total = 50;
 
 % 优化变量
 x = zeros(2, total+1);
@@ -61,46 +61,64 @@ v = zeros(1, total);
 e = zeros(2, total+1);
 
 % 初始值
-x(:, 1) = [1.1; -0.7];
-z(:, 1) = x(:, 1);
+X0 = [-1,1,1,0;-1,1,0,1];
+x_stash = [];
+u_stash = [];
+for it = 1:4
+    x(:, 1) = X0(:, it);
+    z(:, 1) = x(:, 1);
 
-%% 运行
-for i = 1:total
-    t(i) = i - 1;
-    v0 = v(i);
+    %% 运行
+    for i = 1:total
+        t(i) = i - 1;
+        v0 = v(i);
+
+        % 终端不等式约束
+        bin1 =  [u1_terminal_upper; u2_terminal_upper] - A^Nc * z(:,1);
+        bin2 = -[u1_terminal_lower; u2_terminal_lower] + A^Nc * z(:,1);
+        bin = [bin1; bin2];
+        Ain = [Phi(2*Nc-1:2*Nc, :); -Phi(2*Nc-1:2*Nc, :)];
+
+        % 优化
+        U = quadprog(Phi'*QQ*Phi+RR, z(:,i)'*F'*QQ*Phi, Ain, bin, [], [], lb, ub, v0);
+        v(i) = U(1);
+        e(:, i) = x(:, i) - z(:, i);
+        u(i) = v(i) - K*e(:, i);
+
+        % 模型迭代 
+        w = (rand - 0.5) * 0.1;
+        x(:, i+1) = A*x(:, i) + B*u(i) + D*w;
+
+        % 标称模型
+        z(:, i+1) = A*z(:, i) + B*v(i);
+    end
     
-    % 终端不等式约束
-    bin1 =  [u1_terminal_upper; u2_terminal_upper] - A^Nc * z(:,1);
-    bin2 = -[u1_terminal_lower; u2_terminal_lower] + A^Nc * z(:,1);
-    bin = [bin1; bin2];
-    Ain = [Phi(2*Nc-1:2*Nc, :); -Phi(2*Nc-1:2*Nc, :)];
-    
-    % 优化
-    U = quadprog(Phi'*QQ*Phi+RR, z(:,i)'*F'*QQ*Phi, Ain, bin, [], [], lb, ub, v0);
-    v(i) = U(1);
-    e(:, i) = x(:, i) - z(:, i);
-    u(i) = v(i) - K*e(:, i);
-    
-    % 模型迭代 
-    w = (rand - 0.5) * 0.2;
-    x(:, i+1) = A*x(:, i) + B*u(i) + D*w;
-    
-    % 标称模型
-    z(:, i+1) = A*z(:, i) + B*v(i);
+    x_stash = [x_stash; x];
+    u_stash = [u_stash; u];
 end
 
 %% 画图
 figure(1);
 subplot(3, 1, 1);
-plot(t, x(1, 1:total));
+plot(t, x_stash(1, 1:total), t, x_stash(3, 1:total), t, x_stash(5, 1:total), t, x_stash(7, 1:total));
+xlabel('迭代次数');
+ylabel('x_1');
+legend('[-1,-1]','[1,1]','[1,0]','[0,1]');
 hold on;
 grid on;
 
 subplot(3, 1, 2);
-plot(t, x(2, 1:total));
+plot(t, x(2, 1:total), t, x_stash(4, 1:total), t, x_stash(6, 1:total), t, x_stash(8, 1:total));
+xlabel('迭代次数');
+ylabel('x_2');
+legend('[-1,-1]','[1,1]','[1,0]','[0,1]');
 hold on;
 grid on;
 
 subplot(3, 1, 3);
-plot(t, u);
+plot(t, u_stash(1, 1:total), t, u_stash(2, 1:total), t, u_stash(3, 1:total), t, u_stash(4, 1:total));
+xlabel('迭代次数');
+ylabel('u');
+legend('[-1,-1]','[1,1]','[1,0]','[0,1]');
 hold off;
+grid on;
